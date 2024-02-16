b1e44407647a2c29769a522401f2abb6343eec84
amr-galaleldin <amrgalalamr10@gmail.com>
2024-02-16 20:26:39 +0200

    keep init

--- lib/features/quran/data/models/quran_model.dart
+++ lib/features/quran/data/models/quran_model.dart
@@ -1,106 +1,91 @@
 class QuranModel {
-  int id;
-  String name;
-  String nameEn;
-  String nameTranslation;
-  int words;
-  int letters;
-  String type;
-  String typeEn;
-  String ar;
-  String en;
-  List<Array> array;
+  final int id;
+  final String name;
+  final String nameEn;
+  final String nameTranslation;
+  final int words;
+  final int letters;
+  final String type;
+  final String typeEn;
+  final String ar;
+  final String en;
+  final List<Array> array;
 
   QuranModel(
-      {this.id,
-      this.name,
-      this.nameEn,
-      this.nameTranslation,
-      this.words,
-      this.letters,
-      this.type,
-      this.typeEn,
-      this.ar,
-      this.en,
-      this.array});
+      {required this.id,
+      required this.name,
+      required this.nameEn,
+      required this.nameTranslation,
+      required this.words,
+      required this.letters,
+      required this.type,
+      required this.typeEn,
+      required this.ar,
+      required this.en,
+      required this.array});
 
-  QuranModel.fromJson(Map<String, dynamic> json) 
-  
-  {
-    id = json['id'];
-    name = json['name'];
-    nameEn = json['name_en'];
-    nameTranslation = json['name_translation'];
-    words = json['words'];
-    letters = json['letters'];
-    type = json['type'];
-    typeEn = json['type_en'];
-    ar = json['ar'];
-    en = json['en'];
-    if (json['array'] != null) {
-      array = new List<Array>();
-      json['array'].forEach((v) {
-        array.add(new Array.fromJson(v));
-      });
-    }
-  }
+  factory QuranModel.fromJson(Map<String, dynamic> json) => QuranModel(
+      id: json['id'],
+      name: json['name'],
+      nameEn: json['name_en'],
+      nameTranslation: json['name_translation'],
+      words: json['words'],
+      letters: json['letters'],
+      type: json['type'],
+      typeEn: json['type_en'],
+      ar: json['ar'],
+      en: json['en'],
+      array: json['array']);
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['name'] = this.name;
-    data['name_en'] = this.nameEn;
-    data['name_translation'] = this.nameTranslation;
-    data['words'] = this.words;
-    data['letters'] = this.letters;
-    data['type'] = this.type;
-    data['type_en'] = this.typeEn;
-    data['ar'] = this.ar;
-    data['en'] = this.en;
-    if (this.array != null) {
-      data['array'] = this.array.map((v) => v.toJson()).toList();
-    }
-    return data;
-  }
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'name_en': nameEn,
+        'name_translation': nameTranslation,
+        'words': words,
+        'letters': letters,
+        'type': type,
+        'type_en': typeEn,
+        'ar': ar,
+        'en': en,
+        'array': array.map((aya) => aya.toJson()).toList(),
+      };
 }
 
 class Array {
-  int id;
-  String ar;
-  String en;
-  String filename;
-  String path;
-  String dir;
-  int size;
+  final int id;
+  final String ar;
+  final String en;
+  final String filename;
+  final String path;
+  final String dir;
+  final int size;
 
   Array(
-      {this.id,
-      this.ar,
-      this.en,
-      this.filename,
-      this.path,
-      this.dir,
-      this.size});
+      {required this.id,
+      required this.ar,
+      required this.en,
+      required this.filename,
+      required this.path,
+      required this.dir,
+      required this.size});
 
-  Array.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    ar = json['ar'];
-    en = json['en'];
-    filename = json['filename'];
-    path = json['path'];
-    dir = json['dir'];
-    size = json['size'];
-  }
+  factory Array.fromJson(Map<String, dynamic> json) => Array(
+      id: json['id'],
+      ar: json['ar'],
+      en: json['en'],
+      filename: json['filename'],
+      path: json['path'],
+      dir: json['dir'],
+      size: json['size']);
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['ar'] = this.ar;
-    data['en'] = this.en;
-    data['filename'] = this.filename;
-    data['path'] = this.path;
-    data['dir'] = this.dir;
-    data['size'] = this.size;
-    return data;
-  }
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'ar': ar,
+        'en': en,
+        'filename': filename,
+        'path': path,
+        'dir': dir,
+        'size': size,
+      };
 }
