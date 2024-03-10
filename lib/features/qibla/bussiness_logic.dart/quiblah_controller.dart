3544504e7a8a47f87bda301eb2e686fcbcf18dae
DESKTOP-KI4J6QQ\mans <ahmedmoanis2030@gmail.com>
2024-03-10 20:23:26 +0200

    final commit 2

--- lib/features/qibla/bussiness_logic.dart/quiblah_controller.dart
+++ lib/features/qibla/bussiness_logic.dart/quiblah_controller.dart
@@ -1,15 +1,25 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:geolocator/geolocator.dart';
 
 class QuiblahCubit extends Cubit<bool> {
   QuiblahCubit(super.initialState);
   bool hasPremission = false;
+ Position? currentLocation;
+ double? latitude;
+ double? longitude;
 
   Future getPremission() async {
     PermissionStatus locationstatus = await Permission.location.request();
     if (locationstatus == PermissionStatus.granted) {
       hasPremission = true;
+      latitude=currentLocation!.latitude;
+      longitude=currentLocation!.longitude;
+      log(latitude!);
+      log(longitude!);
     }
     if (locationstatus == PermissionStatus.denied) {
       const SnackBar(
