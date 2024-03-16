92a8ee982964e8236ba7fd9a51fd39d74c33b1f0
amr-galaleldin <amrgalalamr10@gmail.com>
2024-03-16 05:37:12 +0200

    handle moshef view

--- lib/features/quran/view/screens/moshaf_view.dart
+++ lib/features/quran/view/screens/moshaf_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:lnastaqim/features/quran/bussniess_logic/quran/quran_cubit.dart';
 import 'package:lnastaqim/features/quran/view/widgets/custom_span.dart';
 
@@ -16,7 +18,7 @@ class MoshafView extends StatelessWidget {
               physics: const ClampingScrollPhysics(),
               itemBuilder: (context, index) {
                 return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  padding:  EdgeInsets.symmetric(horizontal: 12.w),
                   child: MoshafPage(pageIndex: index),
                 );
               })),
@@ -36,33 +38,39 @@ class MoshafPage extends StatelessWidget {
         var cubit = QuranCubit.get(context);
         var pageAyahs =
             cubit.getCurrentPageAyahsSeparatedForBasmalah(pageIndex);
-        return Column(
-          mainAxisSize: MainAxisSize.min,
-          children: List.generate(pageAyahs.length, (i) {
-            final ayahs = pageAyahs[i];
-            return Column(
-              children: [
-                FittedBox(
-                  fit: BoxFit.fitWidth,
-                  child: RichText(
-                    textDirection: TextDirection.rtl,
-                    textAlign: TextAlign.center,
-                    text: TextSpan(
-                        children: List.generate(ayahs.length, (ayahIndex) {
-                      return span(
-                        isFirstAyah: ayahIndex == 0 ? true : false,
-                        text: ayahs[ayahIndex].code_v2,
-                        pageIndex: pageIndex,
-                        fontSize: 100,
-                        surahNum: cubit.getSurahNumberFromPage(pageIndex),
-                        ayahNum: ayahs[ayahIndex].ayahUQNumber,
-                      );
-                    })),
-                  ),
-                )
-              ],
-            );
-          }),
+        return SizedBox(height: Get.height,
+          child: Column(mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: List.generate(pageAyahs.length, (i) {
+              final ayahs = pageAyahs[i];
+              return Column(
+                children: [
+                  FittedBox(
+                    fit: BoxFit.fitWidth,
+                    child: RichText(
+                      textDirection: TextDirection.rtl,
+                      textAlign: TextAlign.center,
+                      text: TextSpan(
+                          style: TextStyle(
+                          height: 1.9.h,
+                          ),
+                          children: List.generate(ayahs.length, (ayahIndex) {
+                            return span(
+                              isFirstAyah: ayahIndex == 0 ? true : false,
+                              text: ayahs[ayahIndex].code_v2,
+                              pageIndex: pageIndex,
+                              fontSize: 100.sp,
+                              
+                              surahNum: cubit.getSurahNumberFromPage(pageIndex),
+                              ayahNum: ayahs[ayahIndex].ayahUQNumber,
+                            );
+                          })),
+                    ),
+                  )
+                ],
+              );
+            }),
+          ),
         );
       },
     );
