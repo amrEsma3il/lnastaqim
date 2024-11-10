f9b0caaca705bf0ef6d7c2a69752fca0fbc3f9d9
Hasnaa <hasnaamostafa88@gmail.com>
2024-11-10 14:53:58 +0200

    ..

--- lib/features/home/views/screens/home_view.dart
+++ lib/features/home/views/screens/home_view.dart
@@ -8,7 +8,6 @@ import 'package:lnastaqim/core/constants/images.dart';
 import 'package:lnastaqim/core/utilits/extensions/arabic_numbers.dart';
 
 import '../../../../config/routing/app_routes_info/app_routes_name.dart';
-import '../../../../core/utilits/controller/deep_link_cubit.dart';
 import '../../../paryer_times/bussniess_logic/date_cubit.dart';
 import '../../../paryer_times/view/widgets/prayers_stepper.dart';
 import '../../../quran/bussniess_logic/moshaf_book_mark_cubit/moshaf_bookmark_cubit.dart';
@@ -28,199 +27,206 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    backgroundColor: const Color(0xfff2f4f9),
-    body: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(
-          child: Stack(
-            children: [
-              Image(
-                width: Get.width,
-                fit: BoxFit.cover,
-                image: const AssetImage(AppImages.homeBackground),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 53.h, left: 17.w),
-                child:
-                    Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-                  const Icon(
-                    Icons.search,
-                    color: Colors.white,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 9.0),
-                    child: GestureDetector(
-                      onTap: () {
-                        Get.toNamed(AppRouteName.notification);
-                      },
-                      child: const Icon(
-                        Icons.notifications_none_outlined,
-                        color: Colors.white,
+      backgroundColor: const Color(0xfff2f4f9),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            child: Stack(
+              children: [
+                Image(
+                  width: Get.width,
+                  fit: BoxFit.cover,
+                  image: const AssetImage(AppImages.homeBackground),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 53.h, left: 17.w),
+                  child:
+                      Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                    const Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 9.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          Get.toNamed(AppRouteName.notification);
+                        },
+                        child: const Icon(
+                          Icons.notifications_none_outlined,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                  ),
-                  const Icon(
-                    Icons.wb_sunny_outlined,
-                    color: Colors.white,
-                  )
-                ]),
-              ),
-              Positioned(
-                  top: 128.h,
-                  left: 19.w,
-                  child: SizedBox(
-                    width: Get.width - 35.w,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          children: [
-                            const Text(
-                              "القراءة الاخيرة",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 21,
-                                  fontWeight: FontWeight.w400,
-                                  wordSpacing: -2),
-                            ),
-                            SizedBox(
-                              height: 2.h,
-                            ),
-                            GestureDetector(
-                              child: Row(
-                                children: [
-                                  BlocBuilder<MoshafBookmarkCubit,
-                                      MoshafBookmarkState>(
-                                    builder: (context, moshafBookmarState) {
-                                      bool isMarkExist = moshafBookmarState
-                                              .isMark &&
-                                          moshafBookmarState.pageNumber != 0;
-                                      int pageNumber =
-                                          moshafBookmarState.pageNumber;
-                                      return GestureDetector(
-                                        onTap: () async {
-                                          if (isMarkExist) {
-                                            //  QuranCubit.get(context).clearScreen(context);
-                                            QuranCubit.get(context)
-                                                    .pageController =
-                                                PageController(
-                                                    initialPage:
-                                                        604 - pageNumber);
-                                            Get.toNamed(AppRouteName.moshaf);
-                                            log(pageNumber.toString());
-                                          }
-                                        },
-                                        child: Text(
-                                          isMarkExist
-                                              ? '${'صفحة رقم'} ${pageNumber.toString().toArabic}'
-                                              : "لا توجد قراه بعد",
-                                          style: const TextStyle(
-                                              color: Colors.white60,
-                                              fontSize: 21,
-                                              fontWeight: FontWeight.w400,
-                                              wordSpacing: 1.9),
-                                        ),
-                                      );
-                                    },
-                                  ),
-                                  SizedBox(
-                                    width: 3.w,
-                                  ),
-                                  const Icon(
-                                    Icons.bookmark_outline,
-                                    color: Colors.white,
-                                    size: 21,
-                                  )
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                        BlocBuilder<DateCubit, Map<String, String>>(
-                          builder: (context, dateState) {
-                            return Column(
+                    const Icon(
+                      Icons.wb_sunny_outlined,
+                      color: Colors.white,
+                    )
+                  ]),
+                ),
+                Positioned(
+                    top: 128.h,
+                    left: 19.w,
+                    child: SizedBox(
+                      width: Get.width - 35.w,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Column(
                               children: [
-                                Padding(
-                                  padding: EdgeInsets.only(right: 5.w),
-                                  child: Text(
-                                    dateState["gregorian"]!,
-                                    style: const TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 21,
-                                        fontWeight: FontWeight.w400,
-                                        wordSpacing: -2),
-                                  ),
+                                const Text(
+                                  "القراءة الاخيرة",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 21,
+                                      fontWeight: FontWeight.w400,
+                                      wordSpacing: -2),
                                 ),
                                 SizedBox(
                                   height: 2.h,
                                 ),
-                                Text(
-                                  dateState["hijri"]!,
-                                  style: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 21,
-                                      fontWeight: FontWeight.w400,
-                                      wordSpacing: 1.9),
+                                GestureDetector(
+                                  child: Row(
+                                    children: [
+                                      BlocBuilder<MoshafBookmarkCubit,
+                                          MoshafBookmarkState>(
+                                        builder: (context, moshafBookmarState) {
+                                          bool isMarkExist = moshafBookmarState
+                                                  .isMark &&
+                                              moshafBookmarState.pageNumber !=
+                                                  0;
+                                          int pageNumber =
+                                              moshafBookmarState.pageNumber;
+                                          return GestureDetector(
+                                            onTap: () async {
+                                              if (isMarkExist) {
+                                                //  QuranCubit.get(context).clearScreen(context);
+                                                QuranCubit.get(context)
+                                                        .pageController =
+                                                    PageController(
+                                                        initialPage:
+                                                            604 - pageNumber);
+                                                Get.toNamed(
+                                                    AppRouteName.moshaf);
+                                                log(pageNumber.toString());
+                                              }
+                                            },
+                                            child: Text(
+                                              isMarkExist
+                                                  ? '${'صفحة رقم'} ${pageNumber.toString().toArabic}'
+                                                  : "لا توجد قراه بعد",
+                                              style: const TextStyle(
+                                                  color: Colors.white60,
+                                                  fontSize: 21,
+                                                  fontWeight: FontWeight.w400,
+                                                  wordSpacing: 1.9),
+                                            ),
+                                          );
+                                        },
+                                      ),
+                                      SizedBox(
+                                        width: 3.w,
+                                      ),
+                                      const Icon(
+                                        Icons.bookmark_outline,
+                                        color: Colors.white,
+                                        size: 21,
+                                      )
+                                    ],
+                                  ),
                                 ),
                               ],
-                            );
-                          },
-                        ),
-                      ],
-                    ),
-                  ))
-            ],
-          ),
-        ),
-        const Expanded(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "مواعيد الصلاوات",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 15),
+                            ),
+                          ),
+                          BlocBuilder<DateCubit, Map<String, String>>(
+                            builder: (context, dateState) {
+                              return Expanded(
+                                child: Column(
+                                  children: [
+                                    Padding(
+                                      padding: EdgeInsets.only(right: 5.w),
+                                      child: Text(
+                                        dateState["gregorian"]!,
+                                        style: const TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 21,
+                                            fontWeight: FontWeight.w400,
+                                            wordSpacing: -2),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 2.h,
+                                    ),
+                                    Text(
+                                      dateState["hijri"]!,
+                                      textAlign: TextAlign.center,
+                                      style: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 21,
+                                          fontWeight: FontWeight.w400,
+                                          wordSpacing: 1.9),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          ),
+                        ],
                       ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      PrayersStepper()
-                    ],
-                  ),
-                ),
-                CarouselSliderAyah(),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "المميزات",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 15),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      FeaturesGridView(),
-                      SizedBox(
-                        height: 30,
-                      ),
-                    ],
-                  ),
-                ),
+                    ))
               ],
             ),
           ),
-        )
-      ],
-    ),
-        );
+          const Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "مواعيد الصلاوات",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 15),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        PrayersStepper()
+                      ],
+                    ),
+                  ),
+                  CarouselSliderAyah(),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "المميزات",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 15),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        FeaturesGridView(),
+                        SizedBox(
+                          height: 30,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'core/utilits/controller/deep_link_cubit.dart';
+import 'features/quran/bussniess_logic/memorized_verse_cubit/memorized_verse_cubit.dart';
 import 'firebase_options.dart';
 
 import 'package:lnastaqim/core/constants/colors.dart';
@@ -18,9 +21,7 @@ import 'core/utilits/services/work_manager_service.dart';
 import 'features/note/bussniess_logic/overlay_note_control/overlay_note_control_cubit.dart';
 import 'features/paryer_times/bussniess_logic/date_cubit.dart';
 
-
 import 'package:hive_flutter/adapters.dart';
-
 import 'package:lnastaqim/features/bookmark/bussniess_logic/bookmark_cubit/bookmark_cubit.dart';
 import 'package:lnastaqim/features/bookmark/data/models/bookmark_model.dart';
 import 'package:lnastaqim/features/note/bussniess_logic/add_note_cubit/add_note_cubit.dart';
@@ -29,20 +30,14 @@ import 'package:lnastaqim/features/note/data/models/note_model.dart';
 import 'package:lnastaqim/features/quran/bussniess_logic/quran/quran_cubit.dart';
 import 'package:lnastaqim/features/tafaseer/bussniess_logic/tafseer_cubit.dart';
 
-import 'config/routing/app_routingconfig/app_router_configuration.dart';
 import 'core/constants/constants.dart';
-
-import 'features/bookmark/bussniess_logic/add_bookmark_cubit/add_bookmark_cubit.dart';
-import 'features/notification/bussiness_logic/notification_cubit.dart';
-
-
 // import 'core/utilits/functions/search_string_pattern/boyer_moore_algo.dart' as boyer;
 import 'features/7adis/bussiness_logic/a7adith_cubit.dart';
 import 'features/azkar_with_sib7a/business_logic/azkar_category_cubit/azkar_category_cubit.dart';
 import 'features/azkar_with_sib7a/business_logic/azkar_details_cubit/azkar_details_cubit.dart';
 import 'features/azkar_with_sib7a/business_logic/shared_azkar_cubit/shared_azkar_cubit.dart';
-
-
+import 'features/bookmark/bussniess_logic/add_bookmark_cubit/add_bookmark_cubit.dart';
+import 'features/notification/bussiness_logic/notification_cubit.dart';
 import 'features/paryer_times/bussniess_logic/prayers_times_cubit.dart';
 import 'features/quran/bussniess_logic/fast_transition/fast_transition_cubit.dart';
 import 'features/quran/bussniess_logic/moshaf_book_mark_cubit/moshaf_bookmark_cubit.dart';
@@ -53,7 +48,6 @@ import 'features/quran/bussniess_logic/screen_tap_Visibility/screen_tap_visabili
 import 'features/quran/bussniess_logic/sowra_detail/sora_details_cubit.dart';
 import 'features/quran_sound/data/models/reciter_entity.dart';
 import 'features/quran_sound/logic/audio_cubit/audio_cubit.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -94,7 +88,9 @@ class Lnastaqim extends StatelessWidget {
       builder: (context, child) {
         return MultiBlocProvider(
           providers: [
-
+            BlocProvider(
+              create: (context) => DeepLinkCubit(),
+            ),
             //SearchOnAyaCubit
             BlocProvider(
               create: (context) => SearchVisabilityCubit(),
@@ -163,17 +159,39 @@ class Lnastaqim extends StatelessWidget {
                     AzkarCategoryCubit()..getAzkarCategory()),
                      BlocProvider(
                 create: (BuildContext context) =>
+                    MemorizedVerseCubit()),
+            BlocProvider(
+                create: (BuildContext context) =>
                     PrayersTimesCubit()..fetchPrayersTimes()),
             BlocProvider(
                 create: (BuildContext context) =>
                     AzkarDetailsCubit()..getAzkarDetails()),
             BlocProvider(create: (BuildContext context) => SharedAzkarCubit()),
-
           ],
-          child: GetMaterialApp(
-            locale: const Locale('ar'),
-            debugShowCheckedModeBanner: false,
-            getPages: routes,
+          child: BlocListener<DeepLinkCubit, Uri?>(
+            listener: (context, deepLink) {
+              if (deepLink != null) {
+                if (deepLink.path == "/moshaf") {
+
+                  int page = int.parse(deepLink.queryParameters["page"]!);
+                  int ayaNum = int.parse(deepLink.queryParameters["verse"]!);
+         
+
+                  QuranCubit.get(context).pageController =
+                      PageController(initialPage: 604 - page);
+                  Get.toNamed(deepLink.path);
+
+                  QuranCubit.get(context).searchAya(ayaNum);
+                } else {
+                  Get.toNamed(deepLink.path);
+                }
+              }
+            },
+            child: GetMaterialApp(
+              locale: const Locale('ar'),
+              debugShowCheckedModeBanner: false,
+              getPages: routes,
+            ),
           ),
         );
       },
