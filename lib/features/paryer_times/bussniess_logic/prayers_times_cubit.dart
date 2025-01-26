53a280bcef85a2509f826c2af77dc3760f77b92b
amr esmail <amrgalalamr10@gmail.com>
2025-01-26 06:22:18 +0200

    change paryer cubit

--- lib/features/paryer_times/bussniess_logic/prayers_times_cubit.dart
+++ lib/features/paryer_times/bussniess_logic/prayers_times_cubit.dart
@@ -4,12 +4,8 @@ import 'dart:developer';
 import 'package:adhan/adhan.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:lnastaqim/features/paryer_times/data/models/prayers_time_model.dart';
 import 'package:lnastaqim/features/paryer_times/data/repository/prayers_times_repo.dart';
-
-import '../../../core/utilits/services/location_service.dart';
-import '../../../main.dart';
 
 
 
@@ -18,35 +14,40 @@ part 'prayers_times_state.dart';
 class PrayersTimesCubit extends Cubit<PrayersTimeModel> {
   PrayersTimesCubit() : super(PrayersTimeModel(asr: "",dhuhr: "",fajr: "",maghrib: "",isha: "",sunrise: "",lastThirdOfTheNight: "",qiblaDirection: 0.0,currentPrayer: PrayerModel(name:"fajr",index: 0 ))){
     fetchPrayersTimes();
+  Timer.periodic(const Duration(minutes: 1), (timer) {
+  fetchPrayersTimes();
+});
+    
   }
 
 
 static PrayersTimesCubit get(BuildContext context)=>BlocProvider.of<PrayersTimesCubit>(context);
-  late Timer timer;
+  // late Timer timer;
 
 
-   static Coordinates myCoordinates = Coordinates(31.053698, 31.409504
-);
+//     final  Coordinates myCoordinates = Coordinates(31.053698, 31.409504
+// );
 
 
 
-  static CalculationParameters params =
-      PrayersTimesRepo.getCalculationParameters();
-  static PrayersTimeModel prayerTimesModel =
-      PrayersTimesRepo.fetchPrayersTimes(myCoordinates, params);
-  fetchPrayersTimes()async{
-    log("test from paryer times");
-// Position? position=await LocationService.determinePosition();
+  //  final CalculationParameters params =
+  //     PrayersTimesRepo.getCalculationParameters();
+   final PrayersTimeModel paryerTimesModel =
+      PrayersTimesRepo.fetchPrayersTimes( Coordinates(31.053698, 31.409504
+), PrayersTimesRepo.getCalculationParameters());
+  fetchPrayersTimes() async {
+    log("Fetching prayer times...");
 
-   emit(prayerTimesModel);
+    // إعادة حساب أوقات الصلاة بناءً على الوقت الحالي
+    final params = PrayersTimesRepo.getCalculationParameters();
+    final newPrayerTimesModel =
+        PrayersTimesRepo.fetchPrayersTimes(Coordinates(31.053698, 31.409504
+), params);
 
-//    timer=Timer.periodic(const Duration(minutes: 1), (timer) {  
-// log(prayerTimesModel.currentPrayer!.index.toString());
-// log(prayerTimesModel.currentPrayer!.name.toString());
+    log("Current prayer index: ${newPrayerTimesModel.currentPrayer?.index}");
+    log("Current prayer name: ${newPrayerTimesModel.currentPrayer?.name}");
 
-//     emit(prayerTimesModel);
-//            log("-----------------test from paryer times-----------------");
-
-// });
+    // تحديث الحالة بالقيم الجديدة
+    emit(newPrayerTimesModel);
   }
 }
